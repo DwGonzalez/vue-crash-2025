68faf73de09bac13a6cfb3f7c65daecb3d4ce3e1
Darwin Gonzalez <darwinmanuel1@hotmail.com>
2025-02-12 20:48:30 -0400

    JobListing component with object prop

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,4 +1,5 @@
 <script setup>
+import JobListing from "@/components/JobListing.vue";
 import jobData from "@/jobs.json";
 import { ref } from "vue";
 
@@ -13,9 +14,7 @@ console.log(jobs.value);
         Browse Jobs
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <div v-for="job in jobs" :key="job.id">
-          {{ job.title }}
-        </div>
+        <JobListing v-for="job in jobs" :key="job.id" :job="job" />
       </div>
     </div>
   </section>
